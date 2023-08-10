e05f45f92439fb8246dd2b52ee62e0a5119831c4
notMrpod <darsh.pod@gmail.com>
2023-08-09 23:03:11 -0500

    File Cleanup

---
+++ twoNumberCalculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class twoNumberCalculator {
+
+    public static void main (String args[]){
+        
+        Scanner console = new Scanner(System.in);
+        
+        System.out.println("What's the first number?");
+        
+    
+    }
+    
+}
