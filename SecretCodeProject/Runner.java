00040d9f2b6cb1ec6a37bd755cd4fb9ca61be11e
notMrpod <darsh.pod@gmail.com>
2023-09-10 09:47:55 -0500

    SecretCode1

--- SecretCodeProject/Runner.java
+++ SecretCodeProject/Runner.java
@@ -15,9 +15,22 @@ public class Runner
         test.concatenate("computer", "science", true);
         
         System.out.println(test.hasSub("Computer science", "sci"));
+        System.out.println("");        
+        System.out.println(test.evenFooBar("foofoobarbarfoo"));
+        System.out.println("");
+        System.out.println(test.evenFooBar("foobarbarfoo"));
+        System.out.println("");
         
-        /* System.out.println(test.evenFooBar(foofoobarbarfoo));
-        System.out.println(test.evenFooBar(foobarbarfoo)); */
+        System.out.println(test.microwaveTime("12"));
+        System.out.println(test.microwaveTime("123"));
+        System.out.println(test.microwaveTime("4500"));
+        System.out.println("");
+        
+        System.out.println(test.reverseWords("hello world"));
+        System.out.println(test.reverseWords("the sky is blue"));
+        System.out.println("");
+        
+        System.out.println(test.sumString("Hi 5 there are 2 or 3 numbers in this String"));
     
     }
     
--- SecretCodeProject/StringMethods.java
+++ SecretCodeProject/StringMethods.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * @author (Darsh)
  */
@@ -54,20 +55,74 @@ public class StringMethods
         }
         return false; // if the loop ends, then it returns false
     
-    }
+    } 
     
-    /* public boolean evenFooBar(String str) {
+    public boolean evenFooBar(String str) {
         
         int strLength = str.length();
-        int fooCounter;
-        int barCounter;
+        int fooCounter = 0;
+        int barCounter = 0;
         
-        for (int i = 0; i < strLength; i++) {
+        for (int i = 0; i < strLength; i = i + 3) {
         
-            
+            String idk = str.substring(i, i+3);
+            boolean temp = idk.equals("foo");
+            if (temp) {
+                fooCounter++;
+            }
+            else {
+                barCounter++;
+            }
 
         }
+        return barCounter == fooCounter;
+    }
+    
+    public String microwaveTime(String str) {
+        switch (str.length()) {
         
-    } */
+            case 1:
+                return "0:0" + str;
+            case 2:
+                return "0:" + str;
+            case 3:
+                String x = str.substring(0, 1);
+                String y = str.substring(1, 2);
+                String z = str.substring(2);
+                return x + ":" + y + z;
+            case 4:
+                String a = str.substring(0 ,1);
+                String b = str.substring(1, 2);
+                String c = str.substring(2, 3);
+                String d = str.substring(3);
+                return a + b + ":" + c + d;
+            default:
+                return "invalid input";
+        
+        }
+    }
+    
+    public String reverseWords(String str) {
+        String[] a = str.split(" ");
+        String b = "";
+        for (int i = 0; i < a.length; i++) {
+            b = a[i] + " " + b;
+        }
+        return b;
+    }
+    
+    public int sumString(String str) {
+        Scanner x = new Scanner(str);
+        String count = "";
+        while (x.hasNext()) {
+            if (x.hasNextInt()) {
+                count = count + x.nextInt();
+            }
+            else {
+                x.next();
+            }
+        }
+        return count.length();
+    }
         
 }
